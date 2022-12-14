2658fa6b1e153dff0a840067cc699df287f1bdf0
Syscily Brown <syscilybrown@syscilys-mbp.myfiosgateway.com>
2022-12-14 17:11:19 -0500

    heroku db

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ app = Flask(__name__)
 """testing git"""
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///recipeapp_db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zlmweucpvqeyas:1d69e4591f63fb6806a9293e8e39aabfa60bf9ffebced4f50bb7294aa527c165@ec2-54-160-109-68.compute-1.amazonaws.com:5432/ddsks4967o2j18'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://eccwayhtoudysw:604ca819180f650a711b6887824127b46b183e65046d675592eb039a931be902@ec2-52-201-124-168.compute-1.amazonaws.com:5432/d57ieskiq15l47'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zlmweucpvqeyas:1d69e4591f63fb6806a9293e8e39aabfa60bf9ffebced4f50bb7294aa527c165@ec2-54-160-109-68.compute-1.amazonaws.com:5432/ddsks4967o2j18'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://eccwayhtoudysw:604ca819180f650a711b6887824127b46b183e65046d675592eb039a931be902@ec2-52-201-124-168.compute-1.amazonaws.com:5432/d57ieskiq15l47'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False 
 app.config['SECRET_KEY'] = API_SECRET_KEY
